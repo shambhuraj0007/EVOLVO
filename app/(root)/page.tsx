fffccdbf7762d7f168dade4312549d639a6b1aab
shambhuraj <shambhuraj960410054@gmail.com>
2025-08-30 14:24:54 +0530

    interview has been generated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,128 +1,64 @@
 import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import InterviewCard from '@/components/InterviewCard'
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+import InterviewCard from "@/components/InterviewCard";
+import {getCurrentUser, getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/auth.action";
 
+const Page = async () => {
+    const user = await getCurrentUser();
 
-const page = () => {
-    // Dummy data for testing
-const hasPastInterviews = [
-  {
-    id: "1",
-    role: "Frontend Developer",
-    type: "Technical",
-    techstack: ["React", "Next.js", "TailwindCSS"],
-    createdAt: "2025-08-01T12:00:00Z",
-  },
-  {
-    id: "2",
-    role: "Backend Engineer",
-    type: "Behavioural",
-    techstack: ["Node.js", "Express", "MongoDB"],
-    createdAt: "2025-08-15T09:30:00Z",
-  },
-  {
-    id: "3",
-    role: "Full Stack Developer",
-    type: "Mixed",
-    techstack: ["React", "Node.js", "PostgreSQL"],
-    createdAt: "2025-08-20T15:45:00Z",
-  },
-];
-// Dummy upcoming interviews
-const allInterview = [
-  {
-    id: "101",
-    role: "Machine Learning Engineer",
-    type: "Technical",
-    techstack: ["Python", "TensorFlow", "Scikit-learn"],
-    createdAt: "2025-09-01T10:00:00Z",
-  },
-  {
-    id: "102",
-    role: "UI/UX Designer",
-    type: "Behavioural",
-    techstack: ["Figma", "Adobe XD", "Prototyping"],
-    createdAt: "2025-09-03T14:00:00Z",
-  },
-  {
-    id: "103",
-    role: "DevOps Engineer",
-    type: "Mixed",
-    techstack: ["AWS", "Docker", "Kubernetes"],
-    createdAt: "2025-09-05T09:30:00Z",
-  },
-];
+    const [userInterviews, latestInterviews] = await Promise.all([
+        await getInterviewsByUserId(user?.id!),
+        await getLatestInterviews({ userId: user?.id! })
+    ]);
 
+    const hasPastInterviews = userInterviews?.length > 0;
+    const hasUpcomingInterviews = latestInterviews?.length > 0;
 
-  return (
-    <>
-      <section className="card-cta">
-        <div className="flex flex-col gap-6 max-w-lg">
-          <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
-          <p className="text-lg">
-            Practice real interview questions & get instant feedback
-          </p>
+    return (
+        <>
+            <section className="card-cta">
+                <div className="flex flex-col gap-6 max-w-lg">
+                    <h2>Get Interview-Ready with AI-Powered Practice & Feedback</h2>
+                    <p className="text-lg">
+                        Practice on real interview questions & get instant feedback
+                    </p>
+                    
+                    <Button asChild className="btn-primary max-sm:w-full">
+                <Link href="/interview">Start an Interview</Link>
+                    </Button>
+                </div>
 
-          <Button asChild className="btn-primary max-sm:w-full">
-            <Link href="/interview">Start an Interview</Link>
-          </Button>
-        </div>
-        <Image
-          src="/robot.png"
-          alt="robo-dude"
-          width={400}
-          height={400}
-          className="max-sm:hidden"
-        />
-      </section>
+                <Image src="/robot.png" alt="robo-dude" width={400} height={400} className="max-sm:hidden" />
+            </section>
 
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Your Interviews</h2>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Your Interviews</h2>
 
-        <div className="interviews-section">
-          {hasPastInterviews.length ? (
-            hasPastInterviews?.map((interview) => (
-              <InterviewCard
-                key={interview.id}
-                userId={interview?.id}
-                interviewId={interview.id}
-                role={interview.role}
-                type={interview.type}
-                techstack={interview.techstack}
-                createdAt={interview.createdAt}
-              />
-            ))
-          ) : (
-            <p>You haven&apos;t taken any interviews yet</p>
-          )}
-        </div>
-      </section>
+                <div className="interviews-section">
+                    {hasPastInterviews ? (
+                        userInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                            <p>You haven&apos;t taken any interviews yet</p>
+                    )}
+                </div>
+            </section>
 
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Take Interviews</h2>
+            <section className="flex flex-col gap-6 mt-8">
+                <h2>Take an Interview</h2>
 
-        <div className="interviews-section">
-         {allInterview ? (
-  allInterview.map((interview) => (
-    <InterviewCard
-      key={interview.id}
-      userId="dummyUser123"
-      interviewId={interview.id}
-      role={interview.role}
-      type={interview.type}
-      techstack={interview.techstack}
-      createdAt={interview.createdAt}
-    />
-  ))
-) : (
-  <p>No upcoming interviews.</p>
-)}
-        </div>
-      </section>
-    </>
-  );
+                <div className="interviews-section">
+                    {hasUpcomingInterviews ? (
+                        latestInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))) : (
+                        <p>There are no new interviews available</p>
+                    )}
+                </div>
+            </section>
+        </>
+    )
 }
-
-export default page
+export default Page
--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,8 +5,8 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
-// import { vapi } from "@/lib/vapi.sdk";
-// import { interviewer } from "@/constants";
+import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
 import { createFeedback } from "@/lib/actions/general.action";
 
 enum CallStatus {
@@ -16,142 +16,141 @@ enum CallStatus {
   FINISHED = "FINISHED",
 }
 
-// interface SavedMessage {
-//   role: "user" | "system" | "assistant";
-//   content: string;
-// }
+interface SavedMessage {
+  role: "user" | "system" | "assistant";
+  content: string;
+}
 
- const Agent = ({
-   userName,
-//   userId,
-//   interviewId,
-//   feedbackId,
-//   type,
-//   questions,
- }: AgentProps) => {
-//   const router = useRouter();
-//   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
-//   const [messages, setMessages] = useState<SavedMessage[]>([]);
-//   const [isSpeaking, setIsSpeaking] = useState(false);
-//   const [lastMessage, setLastMessage] = useState<string>("");
+const Agent = ({
+  userName,
+  userId,
+  interviewId,
+  feedbackId,
+  type,
+  questions,
+}: AgentProps) => {
+  const router = useRouter();
+  const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [lastMessage, setLastMessage] = useState<string>("");
 
-//   useEffect(() => {
-//     const onCallStart = () => {
-//       setCallStatus(CallStatus.ACTIVE);
-//     };
+  useEffect(() => {
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
 
-//     const onCallEnd = () => {
-//       setCallStatus(CallStatus.FINISHED);
-//     };
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
 
-//     const onMessage = (message: Message) => {
-//       if (message.type === "transcript" && message.transcriptType === "final") {
-//         const newMessage = { role: message.role, content: message.transcript };
-//         setMessages((prev) => [...prev, newMessage]);
-//       }
-//     };
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
 
-//     const onSpeechStart = () => {
-//       console.log("speech start");
-//       setIsSpeaking(true);
-//     };
+    const onSpeechStart = () => {
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
 
-//     const onSpeechEnd = () => {
-//       console.log("speech end");
-//       setIsSpeaking(false);
-//     };
+    const onSpeechEnd = () => {
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
 
-//     const onError = (error: Error) => {
-//       console.log("Error:", error);
-//     };
+    const onError = (error: Error) => {
+      console.log("Error:", error);
+    };
 
-//     vapi.on("call-start", onCallStart);
-//     vapi.on("call-end", onCallEnd);
-//     vapi.on("message", onMessage);
-//     vapi.on("speech-start", onSpeechStart);
-//     vapi.on("speech-end", onSpeechEnd);
-//     vapi.on("error", onError);
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
 
-//     return () => {
-//       vapi.off("call-start", onCallStart);
-//       vapi.off("call-end", onCallEnd);
-//       vapi.off("message", onMessage);
-//       vapi.off("speech-start", onSpeechStart);
-//       vapi.off("speech-end", onSpeechEnd);
-//       vapi.off("error", onError);
-//     };
-//   }, []);
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
 
-//   useEffect(() => {
-//     if (messages.length > 0) {
-//       setLastMessage(messages[messages.length - 1].content);
-//     }
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLastMessage(messages[messages.length - 1].content);
+    }
 
-//     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
-//       console.log("handleGenerateFeedback");
+    const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+      console.log("handleGenerateFeedback");
 
-//       const { success, feedbackId: id } = await createFeedback({
-//         interviewId: interviewId!,
-//         userId: userId!,
-//         transcript: messages,
-//         feedbackId,
-//       });
+      const { success, feedbackId: id } = await createFeedback({
+        interviewId: interviewId!,
+        userId: userId!,
+        transcript: messages,
+        feedbackId,
+      });
 
-//       if (success && id) {
-//         router.push(`/interview/${interviewId}/feedback`);
-//       } else {
-//         console.log("Error saving feedback");
-//         router.push("/");
-//       }
-//     };
+      if (success && id) {
+        router.push(`/interview/${interviewId}/feedback`);
+      } else {
+        console.log("Error saving feedback");
+        router.push("/");
+      }
+    };
 
-//     if (callStatus === CallStatus.FINISHED) {
-//       if (type === "generate") {
-//         router.push("/");
-//       } else {
-//         handleGenerateFeedback(messages);
-//       }
-//     }
-//   }, [messages, callStatus, feedbackId, interviewId, router, type, userId]);
+    if (callStatus === CallStatus.FINISHED) {
+      if (type === "generate") {
+        router.push("/");
+      } else {
+        handleGenerateFeedback(messages);
+      }
+    }
+  }, [messages, callStatus, feedbackId, interviewId, router, type, userId]);
 
-//   const handleCall = async () => {
-//     setCallStatus(CallStatus.CONNECTING);
+const handleCall = async () => {
+    setCallStatus(CallStatus.CONNECTING);
 
-//     if (type === "generate") {
-//       await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
-//         variableValues: {
-//           username: userName,
-//           userid: userId,
-//         },
-//       });
-//     } else {
-//       let formattedQuestions = "";
-//       if (questions) {
-//         formattedQuestions = questions
-//           .map((question) => `- ${question}`)
-//           .join("\n");
-//       }
+    if (type === "generate") {
+      await vapi.start(
+        undefined,
+        undefined,
+        undefined,
+        process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!,
+        {
+          variableValues: {
+            username: userName,
+            userid: userId,
+          },
+        }
+      );
+    } else {
+      let formattedQuestions = "";
+      if (questions) {
+        formattedQuestions = questions
+          .map((question) => `- ${question}`)
+          .join("\n");
+      }
 
-//       await vapi.start(interviewer, {
-//         variableValues: {
-//           questions: formattedQuestions,
-//         },
-//       });
-//     }
-//   };
+      await vapi.start(interviewer, {
+        variableValues: {
+          questions: formattedQuestions,
+        },
+      });
+    }
+  };
 
-//   const handleDisconnect = () => {
-//     setCallStatus(CallStatus.FINISHED);
-//     vapi.stop();
-//   };
+  const handleDisconnect = () => {
+    setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
+  };
 
-const isSpeaking = true;
-const callStatus = CallStatus.FINISHED;
-const messages = [
-  "what is your name?",
-  "my name is john doe how are you doing today?"
-];
-const lastMessage = messages[messages.length - 1];
   return (
     <>
       <div className="call-view">
